3fb507e79756a4cc6da208ac9113b3801440afbd
Alfredo <alfredo.austin@ui.ac.id>
2023-05-23 12:56:14 +0700

    add static_root

--- TK/settings.py
+++ TK/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
