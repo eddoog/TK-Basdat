18bf9aa49e4cfba94306a8b98bb66115d04b0ceb
farizedaandhika21 <farizedaandhika21@gmail.com>
2023-05-24 13:30:34 +0700

    query.py

--- manajer/views.py
+++ manajer/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.db import connection
+from django.shortcuts import render, redirect
+from utils.query import query
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+import datetime;
 
 # Create your views here.
 def listPertandingan(request):
@@ -15,3 +20,18 @@ def pesanStadium(request):
 
 def pilihWaktuStadium(request):
     return render(request, 'pilih-waktu-stadium.html')
+
+def historyRapatCode(request):
+    history_rapat = query(f"SELECT * FROM RAPAT")
+    context = {
+        'history_rapat': history_rapat
+    }
+    return render(request, 'history-rapat.html', context)
+
+def listPertandinganCode(request):
+    history_rapat = query(f"SELECT * FROM PERTANDINGAN")
+    context = {
+        'list_pertandingan': list_pertandingan
+    }
+    return render(request, 'list-pertandingan.html', context)
+
