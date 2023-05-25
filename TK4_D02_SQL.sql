a51c424e9b07bbfdccc77c9cf0a2f8228ce7c6a3
Alanna <alanna@ui.ac.id>
2023-05-25 19:17:53 +0700

    Selesai pink alanna

--- TK4_D02_SQL.sql
+++ TK4_D02_SQL.sql
@@ -24,6 +24,33 @@
 -- CRU Mengelola tim (Hijau)
 
 -- CRU Peminjaman Stadium & CR Mulai Rapat (Merah)
+CREATE OR REPLACE FUNCTION check_stadium_availability() RETURNS trigger AS
+$$
+BEGIN
+    IF EXISTS (
+        SELECT *
+        FROM (
+                 SELECT start_datetime, end_datetime, stadium
+                 FROM D02.PERTANDINGAN
+                 UNION SELECT start_datetime, end_datetime, id_stadium
+                 FROM D02.PEMINJAMAN
+             ) temp
+        WHERE temp.stadium = NEW.id_stadium
+          AND start_datetime <= NEW.end_datetime
+          AND end_datetime >= NEW.start_datetime
+    )
+    THEN
+        RAISE EXCEPTION 'Stadium yang dipilih tidak tersedia pada tanggal yang tersebut';
+    END IF;
+    RETURN NEW;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER stadium_availability
+    BEFORE INSERT OR UPDATE ON D02.PEMINJAMAN
+    FOR EACH ROW
+EXECUTE FUNCTION check_stadium_availability();
 
 -- CRUD Pembuatan Pertandingan (Biru)
 
